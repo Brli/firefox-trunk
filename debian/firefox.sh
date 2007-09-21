52641784ecc353caa2530eb10760349694c0315b
Fabien Tassin <fta@sofaraway.org>
2007-09-21 22:15:53 +0000

    * Add a migration profile script:   add debian/firefox.sh, update debian/firefox-3.0.install   debian/firefox-3.0.links

---
+++ debian/firefox.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Script written by Fabien Tassin <fta@sofaraway.org> in September 2007
+
+# If there's still no ~/mozilla/firefox-3.0 profile, try to find a previous
+# firefox profile and initialize with that. We'll try to first use
+# 'firefox-trunk' profile, then try 'grandparadiso', then # plain 'firefox'.
+# If nothing is found, we'll go for a fresh run and let firefox create a
+# default profile for us.
+
+APPNAME=firefox-3.0
+MOZDIR=$HOME/.mozilla
+
+if [ ! -d $MOZDIR/$APPNAME ] ; then
+  FOUND=""
+  if [ -d $MOZDIR/firefox-trunk ] ; then
+    FOUND=firefox-trunk
+  elif [ -d $MOZDIR/granparadiso ] ; then
+    FOUND=granparadiso
+  elif [ -Ãd $MOZDIR/firefox ] ; then
+    FOUND=firefox
+  fi
+
+  if [ "$FOUND" != "" ] ; then
+    echo "*NOTICE* No previous $APPNAME profile found, we'll initialize a profile using a copy of your existing '$FOUND' profile."
+    echo -n "Transfering..."
+    cp -a $MOZDIR/$FOUND $MOZDIR/$APPNAME
+    echo " done."
+  else
+    echo "*NOTICE* No previous firefox profile found, starting with a fresh one"
+  fi
+fi
+
+exec /usr/lib/$APPNAME/$APPNAME "$@"
