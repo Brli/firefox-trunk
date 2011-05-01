8e803af5ec7ac56c87aa7f93e0e03dc28cc49d42
Chris Coulson <chrisccoulson@ubuntu.com>
2011-05-01 13:42:41 +0100

    Ensure the language packs have a tightly versioned dependency on Firefox

--- debian/control.langpacks.pp
+++ debian/control.langpacks.pp
@@ -1,7 +1,7 @@
 
 Package: @APPNAME@-locale-@LANGCODE@
 Architecture: all
-Depends: @APPNAME@, ${misc:Depends}
+Depends: @APPNAME@ (= ${binary:Version}), ${misc:Depends}
 Description: @LANG@ language pack for Firefox
  This package contains @LANG@ translations and search plugins
  for Firefox
