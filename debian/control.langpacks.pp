aada36dfb914f0767dcea832708322e8f5ed6481
Chris Coulson <chrisccoulson@ubuntu.com>
2011-05-04 14:23:59 +0100

    Remove the tight depends on the language packs. Lintian hates me for this

--- debian/control.langpacks.pp
+++ debian/control.langpacks.pp
@@ -1,7 +1,7 @@
 
 Package: @APPNAME@-locale-@LANGCODE@
 Architecture: all
-Depends: @APPNAME@ (= ${binary:Version}), ${misc:Depends}
+Depends: @APPNAME@, ${misc:Depends}
 Description: @LANG@ language pack for Firefox
  This package contains @LANG@ translations and search plugins
  for Firefox
