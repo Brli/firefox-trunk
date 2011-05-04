a0dd5ee17204ce72358a1e3c1aa95b7dc7e519d6
Chris Coulson <chrisccoulson@ubuntu.com>
2011-05-04 13:34:15 +0100

    Oops, add missing file

---
+++ debian/vendor.js
@@ -0,0 +1,16 @@
+// Use LANG environment variable to choose locale
+pref("intl.locale.matchOS", true);
+
+// Disable default browser checking.
+pref("browser.shell.checkDefaultBrowser", false);
+
+// Prevent EULA dialog to popup on first run
+pref("browser.EULA.override", true);
+
+// identify default locale to use if no /usr/lib/firefox-addons/searchplugins/LOCALE
+// exists for the current used LOCALE
+pref("distribution.searchplugins.defaultLocale", "en-US");
+
+// Enable the NetworkManager integration
+pref("network.manage-offline-status", true);
+
