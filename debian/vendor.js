37ca0fae975ff7cc358472bc6241ebfe45d77041
Chris Coulson <chrisccoulson@ubuntu.com>
2011-07-28 00:28:44 +0100

    * Drop a distro pref that should have gone a long time ago   - update debian/vendor.js

--- debian/vendor.js
+++ debian/vendor.js
@@ -4,9 +4,6 @@ pref("intl.locale.matchOS", true);
 // Disable default browser checking.
 pref("browser.shell.checkDefaultBrowser", false);
 
-// Prevent EULA dialog to popup on first run
-pref("browser.EULA.override", true);
-
 // identify default locale to use if no /usr/lib/firefox-addons/searchplugins/LOCALE
 // exists for the current used LOCALE
 pref("distribution.searchplugins.defaultLocale", "en-US");
