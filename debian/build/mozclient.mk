0f624dabb10e83215964e0f191063327203573ec
Chris Coulson <chrisccoulson@ubuntu.com>
2011-09-02 12:11:18 +0100

    Drop mozilla-devscripts part 1 - don't use the compare target. The list of files hasn't been updated in ages anyway, and we can probably achieve the same effect with --list-missing

--- debian/build/mozclient.mk
+++ debian/build/mozclient.mk
@@ -18,35 +18,3 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-13.7 USA.
 
 include /usr/share/mozilla-devscripts/mozclient.mk
-
-COMPARE_FILTER_PRE_IN := sed \
-	-e 's,foo,bar,' \
-	$(NULL)
-
-COMPARE_FILTER_PRE_OUT := sed \
-	-e 's,^usr/lib/firefox-trunk[^/]*/,,' \
-        $(NULL)
-
-COMPARE_FILTER_IN    := sed \
-	-e 's,^usr/lib/firefox-trunk[^/]*/searchplugins,usr/lib/firefox-addons/searchplugins,' \
-	-e 's,^usr/lib/firefox-trunk[^/]*/extensions/inspector@mozilla.org/.*,,' \
-	-e 's,^usr/lib/firefox-trunk[^/]*/extensions,usr/lib/xulrunner-addons/extensions,' \
-	-e 's,^usr/lib/firefox-trunk[^/]*/defaults/profile,etc/firefox-trunk/profile,' \
-	-e 's,^usr/lib/firefox-trunk[^/]*/\(old-homepage-default.properties\|README.txt\|removed-files\),,' \
-	-e 's,^usr/lib/firefox-trunk[^/]*/.autoreg,,' \
-	-e 's,^etc/firefox-trunk[^/]*/.autoreg,,' \
-	$(NULL)
-
-COMPARE_FILTER_OUT   := sed \
-	-e 's,^DEBIAN/.*,,' \
-	-e 's,^usr/lib/debug/.*,,' \
-	-e 's,^usr/share/doc/.*,,' \
-	-e 's,^usr/share/menu/.*,,' \
-	-e 's,^usr/share/applications/.*,,' \
-	-e 's,^usr/share/bug/firefox-trunk/presubj,,' \
-	-e 's,^etc/firefox-trunk/\(firefoxrc\|pref/firefox.js\),,' \
-	-e 's,^usr/lib/firefox-addons/searchplugins/\(debsearch\|wikipedia\).\(gif\|src\),,' \
-	-e 's,^usr/lib/firefox-trunk[^/]*/\(firefox-trunk-restart-required.update-notifier\|firefox.cfg\|firefox.sh\|ffox-4-beta-profile-migration-dialog\),,' \
-	$(NULL)
-
-include /usr/share/mozilla-devscripts/compare.mk
