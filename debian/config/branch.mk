26678e256de828b11c27fe5b55a874c6c6997a80
Chris Coulson <chris.coulson@canonical.com>
2011-10-18 12:50:42 +0100

    Turn off the crash reporter for now

--- debian/config/branch.mk
+++ debian/config/branch.mk
@@ -2,10 +2,5 @@ CHANNEL			= nightly
 MOZ_WANT_UNIT_TESTS	= 1
 NO_AUTO_REFRESH_LOCALES	= 1
 
-ifeq (1,$(shell test "$(DISTRIB_VERSION_MAJOR)$(DISTRIB_VERSION_MINOR)" -ge "1104" && echo "1"))
-# Enable crashreporter on nightly builds newer than Maverick
-MOZ_ENABLE_BREAKPAD = 1
-endif
-
 MOZILLA_REPO = http://hg.mozilla.org/mozilla-central
 L10N_REPO = http://hg.mozilla.org/l10n-central
