94edc41ee68327a26ec7ff7e00f24c5d58878c14
Chris Coulson <chris.coulson@canonical.com>
2012-11-15 19:17:15 +0000

    * Add support for autopkgtest, package the xpcshell, reftest and   mochitest testsuites and migrate the xpcshell-tests, reftest, crashtest,   jstestbrowser and mochitest targets to this   - add firefox-testsuite package in debian/control.in and drop     dbus-x11 and xvfb build depends   - drop xpcshell-tests, reftest, crashtest, jstestbrowser and mochitest     targets from debian/build/testsuite.mk   - add debian/tests/mochitest   - add debian/testscripts/run_mochitest.in * Don't write a log file to the script directory from mochitest   - add debian/patches/mochitest-optional-websock-log.patch   - update debian/patches/series.in

--- debian/build/testsuite.mk
+++ debian/build/testsuite.mk
@@ -2,7 +2,7 @@
 
 ifeq (1,$(MOZ_WANT_UNIT_TESTS))
 ifeq (,$(MOZ_TESTS))
-TESTS = check mochitest
+TESTS = check
 else
 TESTS = $(MOZ_TESTS)
 endif
@@ -12,11 +12,8 @@ run-tests: $(addprefix debian/stamp-,$(TESTS))
 
 $(addprefix debian/stamp-,$(TESTS)): debian/stamp-makefile-build
 
-# Tests that need a X server
-$(addprefix debian/stamp-,mochitest): WRAPPER = xvfb-run -a -s "-screen 0 1024x768x24" dbus-launch --exit-with-session
-
 # Run the test!
 $(addprefix debian/stamp-,$(TESTS)):
 	HOME="$(CURDIR)/$(MOZ_DISTDIR)" \
-	$(WRAPPER) $(MAKE) -C $(CURDIR)/$(MOZ_OBJDIR) $(subst debian/stamp-,,$@) || true
+	$(MAKE) -C $(CURDIR)/$(MOZ_OBJDIR) $(subst debian/stamp-,,$@) || true
 	touch $@
