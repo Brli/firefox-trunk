18ec71560d7cc4235f50c6ec5898165b7e52f62c
Chris Coulson <chris.coulson@canonical.com>
2012-11-15 10:55:29 +0000

    and migrate the xpcshell-tests, reftest, crashtest and jstestbrowser   targets to this   - drop xpcshell-tests, reftest, crashtest and jstestbrowser targets     from debian/build/testsuite.mk   - add debian/tests/crashtest   - add debian/tests/jstestbrowser   - add debian/tests/reftest

--- debian/build/testsuite.mk
+++ debian/build/testsuite.mk
@@ -1,11 +1,8 @@
 #!/usr/bin/make -f
 
-LOCALE		:= en_US.UTF-8
-LOCDIR		= $(CURDIR)/$(MOZ_DISTDIR)/.locales
-
 ifeq (1,$(MOZ_WANT_UNIT_TESTS))
 ifeq (,$(MOZ_TESTS))
-TESTS = check jstestbrowser reftest crashtest mochitest
+TESTS = check mochitest
 else
 TESTS = $(MOZ_TESTS)
 endif
@@ -15,20 +12,8 @@ run-tests: $(addprefix debian/stamp-,$(TESTS))
 
 $(addprefix debian/stamp-,$(TESTS)): debian/stamp-makefile-build
 
-# Required for js/src/trace-tests/sunspider/check-date-format-tofte.js
-$(addprefix debian/stamp-,check jstestbrowser): export TZ = :/usr/share/zoneinfo/posix/US/Pacific
-
-$(LOCDIR)/%:
-	mkdir -p $(LOCDIR)
-	localedef -f $(shell echo $(notdir $@) | cut -d '.' -f 2) -i $(shell echo $(notdir $@) | cut -d '.' -f 1) $@
-
-# Setup locales for tests which need it
-$(addprefix debian/stamp-,jstestbrowser reftest): $(LOCDIR)/$(LOCALE)
-$(addprefix debian/stamp-,jstestbrowser reftest): export LOCPATH=$(LOCDIR)
-$(addprefix debian/stamp-,jstestbrowser reftest): export LC_ALL=$(LOCALE)
-
 # Tests that need a X server
-$(addprefix debian/stamp-,jstestbrowser reftest crashtest mochitest): WRAPPER = xvfb-run -a -s "-screen 0 1024x768x24" dbus-launch --exit-with-session
+$(addprefix debian/stamp-,mochitest): WRAPPER = xvfb-run -a -s "-screen 0 1024x768x24" dbus-launch --exit-with-session
 
 # Run the test!
 $(addprefix debian/stamp-,$(TESTS)):
