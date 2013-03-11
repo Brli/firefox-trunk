989c16c4e969ffbe13d5ea4a7f0513cc11d59207
Chris Coulson <chris.coulson@canonical.com>
2013-03-11 16:10:50 +0000

    * Run tests that need a display in an actual session with   gnome-settings-daemon running (plus a WM):   - update debian/testing/testrunhelper.py   - update debian/firefox-testsuite.install.in   - add debian/testing/run-test.desktop   - update debian/testing/run_mochitest.in   - update debian/testing/run_reftest.in   - update debian/testing/run_xpcshell_tests.in   - update debian/control.in

--- debian/testing/testrunhelper.py
+++ debian/testing/testrunhelper.py
@@ -24,12 +24,18 @@ XDG_PUBLICSHARE_DIR="$HOME/Public"
 XDG_DOCUMENTS_DIR="$HOME/Documents"
 XDG_MUSIC_DIR="$HOME/Music"
 XDG_PICTURES_DIR="$HOME/Pictures"
-XDG_VIDEOS_DIR="$HOME/Videos"'''
+XDG_VIDEOS_DIR="$HOME/Videos"''',
+
+  '.config/gnome-session/sessions/test.session':
+'''[GNOME Session]
+Name=Test Session
+RequiredComponents=gnome-settings-daemon;metacity;
+DesktopName=GNOME'''
 }
 
 class TestRunHelper(OptionParser):
 
-  def __init__(self, runner, get_runner_parser_cb, pass_args=[], paths=[], root=None, need_x=False, need_dbus=False, need_wm=False):
+  def __init__(self, runner, get_runner_parser_cb, pass_args=[], paths=[], root=None, need_x=False):
     OptionParser.__init__(self)
 
     assert root != None
@@ -37,19 +43,11 @@ class TestRunHelper(OptionParser):
     self.root = root
     self._pass_args = pass_args
     self._need_x = need_x
-    self._need_dbus = need_dbus
-    self._need_wm = need_wm
-
-    assert not need_wm or (need_wm and need_x)
 
     if self._need_x:
-      self.add_option('--own-display',
-                      action='store_true', dest='wantOwnDisplay', default=False,
+      self.add_option('--own-session',
+                      action='store_true', dest='wantOwnSession', default=False,
                       help='Run the test inside its own X session')
-    if self._need_dbus:
-      self.add_option('--own-session-bus',
-                      action='store_true', dest='wantOwnSessionBus', default=False,
-                      help='Run the test with its own dbus session')
 
     runner = os.path.join(self.root, runner)
     if os.path.isdir(os.path.join(os.path.dirname(__file__), 'python')):
@@ -78,82 +76,87 @@ class TestRunHelper(OptionParser):
   def run(self, defaults=[], pre_run_cb=None):
     (options, args) = self.parse_args()
 
-    respawn_args = []
-    strip_args = []
+    tmphome = None
+    if os.getenv('DESKTOP_AUTOSTART_ID') == None:
+      # If we were started by the session manager, use the current homedir
+      tmphome = tempfile.mkdtemp()
+      for name in skel:
+        os.system('mkdir -p %s' % os.path.dirname(os.path.join(tmphome, name))) 
+        with open(os.path.join(tmphome, name), 'w+') as f:
+          print >>f, skel[name]
 
-    os.putenv('MOZ_PLUGIN_PATH', os.path.join(self.root, 'plugins'))
-
-    if self._need_x and (options.wantOwnDisplay or os.getenv('DISPLAY') == None):
-      respawn_args.extend(['xvfb-run', '-a', '-s', '-screen 0 1024x768x24 -extension MIT-SCREEN-SAVER'])
-      strip_args.append('--own-display')
-    if self._need_dbus and (options.wantOwnSessionBus or os.getenv('DBUS_SESSION_BUS_ADDRESS') == None):
-      respawn_args.extend(['dbus-launch', '--exit-with-session'])
-      strip_args.append('--own-session-bus')
-
-    if len(respawn_args) > 0:
-      import subprocess
-
-      i = 0
-      while len(sys.argv) > 0 and i < len(sys.argv):
-        arg = sys.argv[i]
-        if arg in strip_args:
-          del sys.argv[i]
-          if self.has_option(arg) and self.get_option(arg).action == 'store':
-            del sys.argv[i]
-        else:
-          i+=1
-
-      respawn_args.extend(sys.argv)
-      return subprocess.call(respawn_args, stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr)
-
-    sys.argv = []
-    argv = sys.argv
-    argv.append(self._runner_global['__file__'])
-
-    for arg in self._pass_args:
-      opt = self.get_option(arg)
-      action = opt.action
-      val = getattr(options, opt.dest)
-      if val == opt.default and action != 'append':
-        continue
-      if action == 'store':
-        if val != None:
-          argv.extend([arg, str(val)])
-      elif action == 'store_true':
-        if val == True:
-          argv.append(arg)
-      elif action == 'store_false':
-        if val == False:
-          argv.append(arg)
-      elif action == 'append':
-        if val != None:
-          for v in val:
-            argv.extend([arg, str(v)])
-      else:
-        raise RuntimeError('Unexpected argument type with action "%s"' % action)
-
-    for arg in defaults:
-      if arg not in argv:
-        argv.extend([arg, defaults[arg]() if hasattr(defaults[arg], '__call__') else defaults[arg]])
-
-    if pre_run_cb != None:
-      pre_run_cb(options, args)
-
-    argv.extend(args)
-
-    tmphome = tempfile.mkdtemp()
-    os.environ['HOME'] = tmphome
-    for name in skel:
-      os.mkdir(os.path.dirname(os.path.join(tmphome, name)))
-      with open(os.path.join(tmphome, name), 'w+') as f:
-        print >>f, skel[name]
-
-    if self._need_wm:
-      wm = Popen(['/usr/bin/metacity', '--sm-disable', '--no-composite'])
+      os.environ['HOME'] = tmphome
 
     try:
+      if self._need_x and (options.wantOwnSession or os.getenv('DISPLAY') == None):
+        import subprocess
+
+        i = 0
+        while len(sys.argv) > 0 and len(sys.argv) > i:
+          arg = sys.argv[i]
+          if arg == '--own-session':
+            del sys.argv[i]
+          else:
+            i += 1
+
+        os.environ['UBUNTU_MOZ_TEST_ARGS'] = ' '.join(sys.argv[1:])
+        os.environ['UBUNTU_MOZ_TEST_RUNNER'] = sys.argv[0]
+        respawn_args = ['xvfb-run', '-a', '-s', '-screen 0 1024x768x24 -extension MIT-SCREEN-SAVER', 'dbus-launch', '--exit-with-session', 'gnome-session', '--autostart', os.path.join(self.root, 'autostart'), '--session', 'test']
+        subprocess.call(respawn_args, stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr)
+
+        if not os.path.exists(os.path.join(tmphome, '.test_return')):
+          return 1
+        with open(os.path.join(tmphome, '.test_return'), 'r') as f:
+          return int(f.read().strip())
+
+      os.environ['MOZ_PLUGIN_PATH'] = os.path.join(self.root, 'plugins')
+
+      sys.argv = []
+      argv = sys.argv
+      argv.append(self._runner_global['__file__'])
+
+      for arg in self._pass_args:
+        opt = self.get_option(arg)
+        action = opt.action
+        val = getattr(options, opt.dest)
+        if val == opt.default and action != 'append':
+          continue
+        if action == 'store':
+          if val != None:
+            argv.extend([arg, str(val)])
+        elif action == 'store_true':
+          if val == True:
+            argv.append(arg)
+        elif action == 'store_false':
+          if val == False:
+            argv.append(arg)
+        elif action == 'append':
+          if val != None:
+            for v in val:
+              argv.extend([arg, str(v)])
+        else:
+          raise RuntimeError('Unexpected argument type with action "%s"' % action)
+
+      for arg in defaults:
+        if arg not in argv:
+          argv.extend([arg, defaults[arg]() if hasattr(defaults[arg], '__call__') else defaults[arg]])
+
+      if pre_run_cb != None:
+        pre_run_cb(options, args)
+
+      argv.extend(args)
+
       return self._runner_global['main']()
+
+    except:
+      with open(os.path.join(os.getenv('HOME'), '.test_return'), 'w+') as f:
+        print >>f, '1'
+
     finally:
-      if self._need_wm:
-        wm.terminate()
-      shutil.rmtree(tmphome)
+      if os.getenv('DESKTOP_AUTOSTART_ID') != None:
+        if not os.path.exists(os.path.join(os.getenv('HOME'), '.test_return')):
+          with open(os.path.join(os.getenv('HOME'), '.test_return'), 'w+') as f:
+            print >>f, '0'
+        os.system('gnome-session-quit --logout --no-prompt')
+      elif tmphome != None:
+        shutil.rmtree(tmphome)
