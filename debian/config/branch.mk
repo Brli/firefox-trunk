643da6c2e48714ff6399642fdb33ff745261cdd8
Chris Coulson <chris.coulson@canonical.com>
2016-03-01 21:20:00 +0000

    Enable telemetry

--- debian/config/branch.mk
+++ debian/config/branch.mk
@@ -1,7 +1,7 @@
 CHANNEL			= nightly
 MOZ_WANT_UNIT_TESTS = 0
-MOZ_OFFICIAL_BUILD = 0
-MOZ_ENABLE_TELEMETRY = 0
+MOZ_OFFICIAL_BUILD = 1
+MOZ_ENABLE_TELEMETRY = 1
 
 MOZILLA_REPO = http://hg.mozilla.org/mozilla-central
 L10N_REPO = http://hg.mozilla.org/l10n-central
