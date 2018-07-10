646e6a7cf6c1ee4de259a4c2367a0f8b8ef3b0fa
Rico Tzschichholz <ricotz@ubuntu.com>
2018-07-10 07:45:54 +0200

    * Add patch from bmo: #1464766 to allow addon signature requirements   to be relaxed   - add debian/patches/allow-addon-signature-requirements-to-be-relaxed.patch   - update debian/patches/series * Lock extensions.unsignedScopes to 4 to disable the addon signing   requirement for our language packs. This doesn't affect user installed   addons   - update debian/vendor-gre.js

--- debian/vendor-gre.js
+++ debian/vendor-gre.js
@@ -3,3 +3,6 @@ pref("intl.locale.matchOS", true);
 
 // Enable Network Manager integration
 pref("network.manage-offline-status", true);
+
+// Enable unsigned addons in the application scope
+pref("extensions.unsignedScopes", 4, locked);
