f108debe8da89329e0643fe1ade77b9574c2642c
Rico Tzschichholz <ricotz@ubuntu.com>
2019-08-27 12:48:48 +0200

    * Update cbindgen to 0.9.1   - debian/build/create-tarball.py

--- debian/build/create-tarball.py
+++ debian/build/create-tarball.py
@@ -128,7 +128,7 @@ class TarballCreator(OptionParser):
         CheckCall(['cargo', 'new', 'vendored-cbindgen', '--vcs', 'none'])
         with ScopedWorkingDirectory('vendored-cbindgen'):
           with open('Cargo.toml', 'a+') as fd:
-            fd.write('cbindgen = "=0.9.0"')
+            fd.write('cbindgen = "=0.9.1"')
           CheckCall(['cargo', 'vendor'])
           with ScopedWorkingDirectory('vendor/cbindgen'):
             os.makedirs('.cargo')
