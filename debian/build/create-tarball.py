4eac8f6127d579ba04530aa22fcf50e5a906c36f
Rico Tzschichholz <ricotz@ubuntu.com>
2020-03-24 18:34:27 +0100

    * Update patches   - debian/patches/ubuntu-ua-string-changes.patch   - debian/patches/cleanup-old-distribution-search-engines.patch * Drop obsolete patches   - debian/patches/s390x-authenticator.patch * Fix i386 build   - debian/patches/bug1622013-vpx-mmx.diff * Separately installed browser/blocklist.xml was removed   - debian/firefox.install.in * Update cbindgen to 0.13.2   - debian/build/create-tarball.py * Bump build-dep on nasm >= 2.14.02   - debian/control{,.in}

--- debian/build/create-tarball.py
+++ debian/build/create-tarball.py
@@ -129,7 +129,7 @@ class TarballCreator(OptionParser):
         CheckCall(['cargo', 'new', 'vendored-cbindgen', '--vcs', 'none'])
         with ScopedWorkingDirectory('vendored-cbindgen'):
           with open('Cargo.toml', 'a+') as fd:
-            fd.write('cbindgen = "=0.13.1"')
+            fd.write('cbindgen = "=0.13.2"')
           CheckCall(['cargo', 'vendor'])
           with ScopedWorkingDirectory('vendor/cbindgen'):
             os.makedirs('.cargo')
