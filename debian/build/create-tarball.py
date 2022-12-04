15a32acf75e2dce611010efe2971aef800483287
Rico Tzschichholz <ricotz@ubuntu.com>
2022-12-04 13:58:58 +0100

    Bump dump_sys to 2.1.1

--- debian/build/create-tarball.py
+++ debian/build/create-tarball.py
@@ -154,7 +154,7 @@ class TarballCreator(OptionParser):
         CheckCall(['cargo', 'new', 'vendored-dump_syms', '--vcs', 'none'])
         with ScopedWorkingDirectory('vendored-dump_syms'):
           with open('Cargo.toml', 'a+') as fd:
-            fd.write('dump_syms = "=2.1.0"')
+            fd.write('dump_syms = "=2.1.1"')
           CheckCall(['cargo', 'vendor'])
           with ScopedWorkingDirectory('vendor/dump_syms'):
             os.makedirs('.cargo')
